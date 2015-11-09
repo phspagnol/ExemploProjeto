a057ec3af75b9b6ffc9602d69a32369120ce9641
phspagnol <ph_spagnol@hotmail.com>
2015-11-09 12:00:37 -0200

    Adicionado atributos as Classes Pessoa e Criado metodo principal do programa

--- ExemploJavaGit/src/Pessoa.java
+++ ExemploJavaGit/src/Pessoa.java
@@ -9,5 +9,43 @@
  * @author PHInfoNote
  */
 public class Pessoa {
+    private Integer id;
+    private String nome;
+    private Cidade cidade;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id, String nome, Cidade cidade) {
+        this.id = id;
+        this.nome = nome;
+        this.cidade = cidade;
+    }
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+    
     
 }
--- ExemploJavaGit/src/Principal.java
+++ ExemploJavaGit/src/Principal.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,6 +18,13 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Cidade c= new Cidade(1,"Tapejara", "RS");
+        Pessoa p= new Pessoa(1,"Paulo Henrique", c);
+        String saida=("Nome "+p.getNome()+" Cidade: "+p.getCidade().getNome());
+        
+        JOptionPane.showMessageDialog(null, saida);
+        
+        
     }
     
 }
