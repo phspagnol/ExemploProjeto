66fb4e8136d60f101163cc8b8ccbe95a32fa6246
phspagnol <ph_spagnol@hotmail.com>
2015-11-09 11:37:02 -0200

    Foram Inseridas apenas as classes, porem o metodo principal ficará para uma nova versão.

---
+++ ExemploJavaGit/src/Cidade.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PHInfoNote
+ */
+public class Cidade {
+    private Integer id;
+    private String nome;
+    private String uf;
+
+    public Cidade() {
+    }
+
+    public Cidade(Integer id, String nome, String uf) {
+        this.id = id;
+        this.nome = nome;
+        this.uf = uf;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
+    
+}
